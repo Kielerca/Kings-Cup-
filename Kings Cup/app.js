f856b02254e3d8fd02a67e3d47dc79ee38885212
Kielerca <38112520+Kielerca@users.noreply.github.com>
2020-04-20 20:07:41 -0500

    Add files via upload

--- Kings Cup/app.js
+++ Kings Cup/app.js
@@ -29,7 +29,7 @@ deck = [];
 var LossCount = 52;
 var isGameOver = 0;
 var Loser = "";
-
+var closeToPop = 0;
 
 class Player {
     constructor(id) {
@@ -57,66 +57,67 @@ class Player {
 
 function cardSetup(){
 
-
+       
     //SPADES
-    cardArray[0] = new Card("Ace of Spades"  ,"client/Assets/JPEG/AS.jpg",1,6);
-    cardArray[1] = new Card("King of Spades" ,"client/Assets/JPEG/KS.jpg",1,5);
-    cardArray[3] = new Card("Queen of Spades","client/Assets/JPEG/QS.jpg",1,3);
-    cardArray[2] = new Card("Jack of Spades" ,"client/Assets/JPEG/JS.jpg",1,4);
-    cardArray[4] = new Card("Ten of Spades"  ,"client/Assets/JPEG/10S.jpg",1,2);
-    cardArray[5] = new Card("Nine of Spades" ,"client/Assets/JPEG/9S.jpg",1,1);
-    cardArray[6] = new Card("Eight of Spades" ,"client/Assets/JPEG/8S.jpg",1,1);
-    cardArray[7] = new Card("Seven of Spades" ,"client/Assets/JPEG/7S.jpg",1,1);
-    cardArray[8] = new Card("Six of Spades"   ,"client/Assets/JPEG/6S.jpg",1,1);
-    cardArray[9] = new Card("Five of Spades"  ,"client/Assets/JPEG/5S.jpg",1,1);
-    cardArray[10] = new Card("Four of Spades" ,"client/Assets/JPEG/4S.jpg",1,1);
-    cardArray[11] = new Card("Three of Spades","client/Assets/JPEG/3S.jpg",1,1);
-    cardArray[12] = new Card("Two of Spades"  ,"client/Assets/JPEG/2S.jpg",1,1);
+    cardArray[0] = new Card("Ace of Spades"  ,"client/Assets/JPEG/AS.jpg",1, "Waterfall");
+    cardArray[1] = new Card("King of Spades" ,"client/Assets/JPEG/KS.jpg",1, "Make a Rule");
+    cardArray[3] = new Card("Queen of Spades","client/Assets/JPEG/QS.jpg",1, "Question Master");
+    cardArray[2] = new Card("Jack of Spades" ,"client/Assets/JPEG/JS.jpg",1, "Never Have I Ever ...");
+    cardArray[4] = new Card("Ten of Spades"  ,"client/Assets/JPEG/10S.jpg",1,"Categories");
+    cardArray[5] = new Card("Nine of Spades" ,"client/Assets/JPEG/9S.jpg",1, "Rhyme");
+    cardArray[6] = new Card("Eight of Spades" ,"client/Assets/JPEG/8S.jpg",1,"Mate");
+    cardArray[7] = new Card("Seven of Spades" ,"client/Assets/JPEG/7S.jpg",1,"Heaven");
+    cardArray[8] = new Card("Six of Spades"   ,"client/Assets/JPEG/6S.jpg",1,"Thumb Master");
+    cardArray[9] = new Card("Five of Spades"  ,"client/Assets/JPEG/5S.jpg",1,"Social");
+    cardArray[10] = new Card("Four of Spades" ,"client/Assets/JPEG/4S.jpg",1,"Give 2 Take 2");
+    cardArray[11] = new Card("Three of Spades","client/Assets/JPEG/3S.jpg",1,"Me (Take a drink!)");
+    cardArray[12] = new Card("Two of Spades"  ,"client/Assets/JPEG/2S.jpg",1,"You (Give a drink)");
 
     //CLUBS
-    cardArray[13] = new Card("Ace of Clubs"  ,"client/Assets/JPEG/AC.jpg",2,6);
-    cardArray[14] = new Card("King of Clubs" ,"client/Assets/JPEG/KC.jpg",2,5);
-    cardArray[15] = new Card("Jack of Clubs" ,"client/Assets/JPEG/JC.jpg",2,4);
-    cardArray[16] = new Card("Queen of Clubs","client/Assets/JPEG/QC.jpg",2,3);
-    cardArray[17] = new Card("Ten of Clubs"  ,"client/Assets/JPEG/10C.jpg",2,2);
-    cardArray[18] = new Card("Nine of Clubs" ,"client/Assets/JPEG/9C.jpg",2,1);
-    cardArray[19] = new Card("Eight of Clubs" ,"client/Assets/JPEG/8C.jpg",2,1);
-    cardArray[20] = new Card("Seven of Clubs" ,"client/Assets/JPEG/7C.jpg",2,1);
-    cardArray[21] = new Card("Six of Clubs"   ,"client/Assets/JPEG/6C.jpg",2,1);
-    cardArray[22] = new Card("Five of Clubs"  ,"client/Assets/JPEG/5C.jpg",2,1);
-    cardArray[23] = new Card("Four of Clubs"  ,"client/Assets/JPEG/4C.jpg",2,1);
-    cardArray[24] = new Card("Three of Clubs" ,"client/Assets/JPEG/3C.jpg",2,1);
-    cardArray[25] = new Card("Two of Clubs"   ,"client/Assets/JPEG/2C.jpg",2,1);
+    cardArray[13] = new Card("Ace of Clubs"  ,"client/Assets/JPEG/AC.jpg",2, "Waterfall");
+    cardArray[14] = new Card("King of Clubs" ,"client/Assets/JPEG/KC.jpg",2, "Make a Rule");
+    cardArray[15] = new Card("Jack of Clubs" ,"client/Assets/JPEG/JC.jpg",2, "Question Master");
+    cardArray[16] = new Card("Queen of Clubs","client/Assets/JPEG/QC.jpg",2, "Never Have I Ever ...");
+    cardArray[17] = new Card("Ten of Clubs"  ,"client/Assets/JPEG/10C.jpg",2,"Categories");
+    cardArray[18] = new Card("Nine of Clubs" ,"client/Assets/JPEG/9C.jpg",2, "Rhyme");
+    cardArray[19] = new Card("Eight of Clubs" ,"client/Assets/JPEG/8C.jpg",2,"Mate");
+    cardArray[20] = new Card("Seven of Clubs" ,"client/Assets/JPEG/7C.jpg",2,"Heaven");
+    cardArray[21] = new Card("Six of Clubs"   ,"client/Assets/JPEG/6C.jpg",2,"Thumb Master");
+    cardArray[22] = new Card("Five of Clubs"  ,"client/Assets/JPEG/5C.jpg",2,"Social");
+    cardArray[23] = new Card("Four of Clubs"  ,"client/Assets/JPEG/4C.jpg",2,"Give 2 Take 2");
+    cardArray[24] = new Card("Three of Clubs" ,"client/Assets/JPEG/3C.jpg",2,"Me (Take a drink!)");
+    cardArray[25] = new Card("Two of Clubs"   ,"client/Assets/JPEG/2C.jpg",2,"You (Give a drink)");
 
     //HEARTS
-    cardArray[26] = new Card("Ace of Hearts"  ,"client/Assets/JPEG/AH.jpg",3,6);
-    cardArray[27] = new Card("King of Hearts" ,"client/Assets/JPEG/KH.jpg",3,5);
-    cardArray[28] = new Card("Jack of Hearts" ,"client/Assets/JPEG/JH.jpg",3,4);
-    cardArray[29] = new Card("Queen of Hearts","client/Assets/JPEG/QH.jpg",3,3);
-    cardArray[30] = new Card("Ten of Hearts"  ,"client/Assets/JPEG/10H.jpg",3,2);
-    cardArray[31] = new Card("Nine of Hearts" ,"client/Assets/JPEG/9H.jpg",3,1);
-    cardArray[32] = new Card("Eight of Hearts" ,"client/Assets/JPEG/8H.jpg",3,1);
-    cardArray[33] = new Card("Seven of Hearts" ,"client/Assets/JPEG/7H.jpg",3,1);
-    cardArray[34] = new Card("Six of Hearts"   ,"client/Assets/JPEG/6H.jpg",3,1);
-    cardArray[35] = new Card("Five of Hearts"  ,"client/Assets/JPEG/5H.jpg",3,1);
-    cardArray[36] = new Card("Four of Hearts"  ,"client/Assets/JPEG/4H.jpg",3,1);
-    cardArray[37] = new Card("Three of Hearts" ,"client/Assets/JPEG/3H.jpg",3,1);
-    cardArray[38] = new Card("Two of Hearts"   ,"client/Assets/JPEG/2H.jpg",3,1);
+    cardArray[26] = new Card("Ace of Hearts"  ,"client/Assets/JPEG/AH.jpg",3, "Waterfall");
+    cardArray[27] = new Card("King of Hearts" ,"client/Assets/JPEG/KH.jpg",3, "Make a Rule");
+    cardArray[28] = new Card("Jack of Hearts" ,"client/Assets/JPEG/JH.jpg",3, "Question Master");
+    cardArray[29] = new Card("Queen of Hearts","client/Assets/JPEG/QH.jpg",3, "Never Have I Ever ...");
+    cardArray[30] = new Card("Ten of Hearts"  ,"client/Assets/JPEG/10H.jpg",3,"Categories");
+    cardArray[31] = new Card("Nine of Hearts" ,"client/Assets/JPEG/9H.jpg",3, "Rhyme");
+    cardArray[32] = new Card("Eight of Hearts" ,"client/Assets/JPEG/8H.jpg",3,"Mate");
+    cardArray[33] = new Card("Seven of Hearts" ,"client/Assets/JPEG/7H.jpg",3,"Heaven");
+    cardArray[34] = new Card("Six of Hearts"   ,"client/Assets/JPEG/6H.jpg",3,"Thumb Master");
+    cardArray[35] = new Card("Five of Hearts"  ,"client/Assets/JPEG/5H.jpg",3,"Social");
+    cardArray[36] = new Card("Four of Hearts"  ,"client/Assets/JPEG/4H.jpg",3,"Give 2 Take 2");
+    cardArray[37] = new Card("Three of Hearts" ,"client/Assets/JPEG/3H.jpg",3,"Me (Take a drink!)");
+    cardArray[38] = new Card("Two of Hearts"   ,"client/Assets/JPEG/2H.jpg",3,"You (Give a drink)");
 
     //DIAMONDS
-    cardArray[39] = new Card("Ace of Diamonds"  ,"client/Assets/JPEG/AD.jpg",4,6);
-    cardArray[40] = new Card("King of Diamonds" ,"client/Assets/JPEG/KD.jpg",4,5);
-    cardArray[41] = new Card("Jack of Diamonds" ,"client/Assets/JPEG/JD.jpg",4,4);
-    cardArray[42] = new Card("Queen of Diamonds","client/Assets/JPEG/QD.jpg",4,3);
-    cardArray[43] = new Card("Ten of Diamonds"  ,"client/Assets/JPEG/10D.jpg",4,2);
-    cardArray[44] = new Card("Nine of Diamonds" ,"client/Assets/JPEG/9D.jpg",4,1);
-    cardArray[45] = new Card("Eight of Diamonds" ,"client/Assets/JPEG/8D.jpg",4,1);
-    cardArray[46] = new Card("Seven of Diamonds" ,"client/Assets/JPEG/7D.jpg",4,1);
-    cardArray[47] = new Card("Six of Diamonds"   ,"client/Assets/JPEG/6D.jpg",4,1);
-    cardArray[48] = new Card("Five of Diamonds"  ,"client/Assets/JPEG/5D.jpg",4,1);
-    cardArray[49] = new Card("Four of Diamonds"  ,"client/Assets/JPEG/4D.jpg",4,1);
-    cardArray[50] = new Card("Three of Diamonds" ,"client/Assets/JPEG/3D.jpg",4,1);
-    cardArray[51] = new Card("Two of Diamonds"   ,"client/Assets/JPEG/2D.jpg",4,1);
+    cardArray[39] = new Card("Ace of Diamonds"  ,"client/Assets/JPEG/AD.jpg", 4,"Waterfall");
+    cardArray[40] = new Card("King of Diamonds" ,"client/Assets/JPEG/KD.jpg", 4,"Make a Rule");
+    cardArray[41] = new Card("Jack of Diamonds" ,"client/Assets/JPEG/JD.jpg", 4,"Question Master");
+    cardArray[42] = new Card("Queen of Diamonds","client/Assets/JPEG/QD.jpg", 4,"Never Have I Ever ...");
+    cardArray[43] = new Card("Ten of Diamonds"  ,"client/Assets/JPEG/10D.jpg",4,"Categories");
+    cardArray[44] = new Card("Nine of Diamonds" ,"client/Assets/JPEG/9D.jpg", 4,"Rhyme");
+    cardArray[45] = new Card("Eight of Diamonds" ,"client/Assets/JPEG/8D.jpg",4,"Mate");
+    cardArray[46] = new Card("Seven of Diamonds" ,"client/Assets/JPEG/7D.jpg",4,"Heaven");
+    cardArray[47] = new Card("Six of Diamonds"   ,"client/Assets/JPEG/6D.jpg",4,"Thumb Master");
+    cardArray[48] = new Card("Five of Diamonds"  ,"client/Assets/JPEG/5D.jpg",4,"Social");
+    cardArray[49] = new Card("Four of Diamonds"  ,"client/Assets/JPEG/4D.jpg",4,"Give 2 Take 2");
+    cardArray[50] = new Card("Three of Diamonds" ,"client/Assets/JPEG/3D.jpg",4,"Me (Take a drink!)");
+    cardArray[51] = new Card("Two of Diamonds"   ,"client/Assets/JPEG/2D.jpg",4,"You (Give a drink)");
+
 
 
 
@@ -183,7 +184,7 @@ function assignTurn(players)
     players[0].isTurn = 1
 
     // set loss number 
-    LossCount =  cardIndex = Math.floor(Math.random() * 20) + 20;
+    LossCount =  cardIndex = Math.floor(Math.random() * 20) + 25;
     console.log(LossCount);
 }
 
@@ -195,6 +196,14 @@ function evaluateLoss(name)
         isGameOver = 1;
         Loser = name;
         newCardFlag = -1;
+    }
+    if ((cardStackCount + 20) >= LossCount)
+    {
+        closeToPop = 1;
+    }
+    if ((cardStackCount + 10) >= LossCount)
+    {
+        closeToPop = 2;
     }
 }
 
@@ -305,16 +314,16 @@ setInterval(function(){
 
         if (playerArray.length > 1 && startFlag)
         {
-            //console.log("Updating Card with the " + deck[currIndex].image ) ;
+            //console.log("Updating Card with the " + deck[currIndex].name + deck[currIndex].value  ) ;
             if (newCardFlag)
             {
-                socket.emit('UpdateCard',{data:deck[currIndex].image, card:deck[currIndex]});
+                socket.emit('UpdateCard',{data:deck[currIndex].image, card:deck[currIndex].name,desc:deck[currIndex].value, count:closeToPop });
             }
             if (newCardFlag == 0)
             {
-                socket.emit('UpdateCard',{data:"client/Assets/JPEG/Green_back.jpg", card:deck[currIndex]});
+                socket.emit('UpdateCard',{data:"client/Assets/JPEG/Green_back.jpg", card:"?????", desc:"?????", count:closeToPop});
             }
-            
+
         }
 
         
@@ -332,6 +341,3 @@ setInterval(function(){
 
 
 },1000/23);
-
-
-
